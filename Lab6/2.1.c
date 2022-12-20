9973e3a19e681001e0b6035722435431759e9422
igorokozhukhov <41119305+igorokozhukhov@users.noreply.github.com>
2022-12-20 23:18:47 +0300

    Create 2.1.c

---
+++ Lab6/2.1.c
@@ -0,0 +1,20 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+int main(){
+	pid_t pid = fork();
+	if (pid < -1){
+		fprintf(stderr, "Couldn't fork\n");
+		exit(errno);
+	}
+	if (!pid) printf("Printf from child. PID - %d, PPID - %d\n", getpid(), getppid());
+	else {
+		printf("Printf from parent. PID - %d\n", getpid());
+		int status;
+		waitpid(-1, &status, 0);
+	}
+}
