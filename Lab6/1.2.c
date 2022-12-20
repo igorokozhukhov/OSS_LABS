ccf6ec9f23fc239c0629446f1d15e1dae371b435
igorokozhukhov <41119305+igorokozhukhov@users.noreply.github.com>
2022-12-20 23:17:51 +0300

    Create 1.2.c

---
+++ Lab6/1.2.c
@@ -0,0 +1,6 @@
+int main(int argc, char** argv, char** envp){
+	int i;
+	for (i = 0; *envp && (i++ | 1); envp++){}
+	printf("Number of environment variables: %d\n", i);
+	printf("Number of arguments: %d\n", argc-1);
+}
